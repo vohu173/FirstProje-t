21dbf438bfaaa0d3156ff312c5b6af3361c861d9
vohu <tolmrabota@gmail.com>
2024-05-20 19:54:29 +0300

    PeopleAnimator

---
+++ Assets/TestScript.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Test()
+    {
+        Debug.Log("HAGHA");
+    }
+}
